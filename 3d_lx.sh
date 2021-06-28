5d2c6e5aaaa720b520e9b6aab10708e4b808b88e
mfzhang <1969662442@qq.com>
2021-06-28 16:07:25 +0800

    chetuio

--- 3d_lx.sh
+++
@@ -1,2 +0,0 @@
-roslaunch lxind_nav FR1511H_HW_SIDE_2dnavF.launch 
-
--- middle_shouhu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-roslaunch lxind_middle1 middle_node.launch
-
-while true
-do
-    roslaunch lxind_middle1 middle_node_twice.launch
-    sleep 1
-done
