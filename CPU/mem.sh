d938a8520883ac1d6d01fa4254eb8e106a2a18d5
mfzhang <1969662442@qq.com>
2021-09-14 22:30:47 +0800

    cpu增加温度监控字段

--- CPU/mem.sh
+++ CPU/mem.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 function delFile(){
-    string=`du ~/memlog.txt`
+    string=`du ~/cpu.log`
     #对IFS变量 进行替换处理
     OLD_IFS="$IFS"
     IFS=" "
@@ -19,22 +19,27 @@ function delFile(){
     echo $fileSize
     size=20000
     if [ $fileSize -gt $size ] ; then
-    mv ~/memlog.txt ~/memlog1.txt
+    mv ~/cpu.log ~/cpu.log1
     fi
 }
 
 while true
 do
-    echo "start" >> ~/memlog.txt
+    echo "start" >> ~/cpu.log
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/memlog.txt
-    ps -eo pid,%cpu,%mem,args --sort -%mem | head -5 >> ~/memlog.txt
-    sleep 0.5
+    echo $ttime >> ~/cpu.log
+    ps -eo pid,%cpu,%mem,args --sort -%mem | head -5 >> ~/cpu.log
+    sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/memlog.txt
-    ps -eo pid,%cpu,%mem,args --sort -%cpu | head -5 >> ~/memlog.txt
-    sleep 0.5
-    echo "END" >> ~/memlog.txt
+    echo $ttime >> ~/cpu.log
+    ps -eo pid,%cpu,%mem,args --sort -%cpu | head -5 >> ~/cpu.log 
+    sleep 0.1
+    ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
+    echo $ttime >> ~/cpu.log
+    sensors  >> ~/cpu.log
+    sleep 0.1
+    echo "END" >> ~/cpu.log
+    echo -e " " >> ~/cpu.log
     delFile
-    sleep 0.5
+    sleep 0.1
 done
