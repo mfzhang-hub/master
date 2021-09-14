473af6b3f2d99a2b2f8840a55f246b2c4eb3e189
mfzhang <1969662442@qq.com>
2021-09-14 23:18:20 +0800

    去除文件已经存在告警

--- CPU/mem.sh
+++ CPU/mem.sh
@@ -1,9 +1,11 @@
 
 
 #!/bin/bash
-
+	if [ ! -d "~/cpu/" ];then
+	mkdir -p ~/cpu/
+	fi
 function delFile(){
-    string=`du ~/cpu.log`
+    string=`du ~/cpu/cpu.log`
     #对IFS变量 进行替换处理
     OLD_IFS="$IFS"
     IFS=" "
@@ -25,21 +27,21 @@ function delFile(){
 
 while true
 do
-    echo "start" >> ~/cpu.log
+    echo "start" >> ~/cpu/cpu.log
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu.log
-    ps -eo pid,%cpu,%mem,args --sort -%mem | head -5 >> ~/cpu.log
+    echo $ttime >> ~/cpu/cpu.log
+    ps -eo pid,%cpu,%mem,args --sort -%mem | head -5 >> ~/cpu/cpu.log
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu.log
-    ps -eo pid,%cpu,%mem,args --sort -%cpu | head -5 >> ~/cpu.log 
+    echo $ttime >> ~/cpu/cpu.log
+    ps -eo pid,%cpu,%mem,args --sort -%cpu | head -5 >> ~/cpu/cpu.log
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu.log
-    sensors  >> ~/cpu.log
+    echo $ttime >> ~/cpu/cpu.log
+    sensors  >> ~/cpu/cpu.log
     sleep 0.1
-    echo "END" >> ~/cpu.log
-    echo -e " " >> ~/cpu.log
+    echo "END" >> ~/cpu/cpu.log
+    echo -e " " >> ~/cpu/cpu.log
     delFile
     sleep 0.1
 done
