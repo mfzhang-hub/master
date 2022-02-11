eab1bc7f18ea22f1966745a9b8cd826fbc9dc3df
mfzhang <1969662442@qq.com>
2022-02-11 14:42:09 +0800

    中兴车型下位机程序对应说明

---
+++ zte_stm32_id.h
@@ -0,0 +1,25 @@
+// 
+namespace zte_id_T660SCG    //双层辊筒车型
+{
+    enum {
+      down_01010302100201,  //双层辊筒下层模块
+      up_01010302100301,    //双层辊筒上层模块
+
+    };
+} //
+
+namespace zte_id_deli_double2_roller    //得力定制辊筒车型
+{
+    enum {
+      deli_01010301100401,  //得力辊筒模块-单独控制板
+
+    };
+} //
+
+namespace zte_id_single_roller //单层辊筒模块
+{
+    enum {
+      single_01010301100201,  //单层辊筒模块———260、261
+
+    };
+} //
