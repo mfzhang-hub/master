2d44edb4108541090552126fbbe594a1d0a2b998
mfzhang <1969662442@qq.com>
2022-05-23 20:04:34 +0800

    更新CPU打印脚本，增加到达一定数值后压缩保存

--- cpu.sh
+++ cpu.sh
@@ -1,13 +1,25 @@
 
 
 #!/bin/bash
-save_day=10
-	if [ ! -d "~/cpu/" ];then
-	mkdir -p ~/cpu/
+#save_day=10
+
+
+	if [ ! -d "~/CPU/cpu/" ];then
+	mkdir -p ~/CPU/cpu/
 	fi
+	if [ ! -d "~/CPU/beifen/" ];then
+	mkdir -p ~/CPU/beifen/
+	fi
+	if [ ! -d "~/CPU/log/" ];then
+	mkdir -p ~/CPU/log/
+	fi
+	#rm -r ~/CPU/cpu/*
+    #sleep 0.1
+    rm -r ~/CPU/beifen/*
+    sleep 0.1
 function delFile(){
-    string=`du ~/cpu/cpu.log`
-    string=`du ~/cpu/memory.log`
+    string=`du ~/CPU/cpu/cpu.log`
+    string=`du ~/CPU/cpu/memory.log`
     OLD_IFS="$IFS"
     IFS=" "
     array=($string)
@@ -20,12 +32,23 @@ function delFile(){
     break
     done
     echo $fileSize
-    size=20000000   #可修改配置
+    size=20000
     if [ $fileSize -gt $size ] ; then
-    mv ~/cpu/cpu.log ~/cpu/cpu.log1
+    mv ~/CPU/cpu/cpu.log ~/CPU/cpu/cpu.log1
     fi	
    if [ $fileSize -gt $size ] ; then
-    mv ~/cpu/memory.log ~/cpu/memory.log1
+    mv ~/CPU/cpu/memory.log ~/CPU/cpu/memory.log1
+    fi
+	if [ $fileSize -gt $size ] ; then
+    cp ~/CPU/cpu/* ~/CPU/beifen
+    sleep 1
+    tar -zcPvf ~/CPU/log/log-$(date +%Y-%m-%d-%H-%M).tar.gz ~/CPU/beifen
+    #sleep 0.1    
+    #rm -r ~/CPU/cpu/*
+    sleep 1
+    rm -r ~/CPU/beifen/*
+    sleep 0.1
+    find ~/CPU/log -mtime +1 -name "log-$(date +%Y-%m-%d-%H-%M).tar.gz" -exec rm -rf {} \;
     fi
     
 }
@@ -38,49 +61,49 @@ echo "%us：表示用户空间程序的cpu使用率（没有通过nice调度）
 %wa：cpu运行时在等待io的时间
 %hi：cpu处理硬中断的数量
 %si：cpu处理软中断的数量
-%st：被虚拟机偷走的cpu" >> ~/cpu/cpu.log
+%st：被虚拟机偷走的cpu" >> ~/CPU/cpu/cpu.log
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/memory.log
-    echo "开始查询" >> ~/cpu/memory.log
+    echo $ttime >> ~/CPU/cpu/memory.log
+    echo "开始查询" >> ~/CPU/cpu/memory.log
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/cpu.log
-    echo "开始查询" >> ~/cpu/cpu.log
+    echo $ttime >> ~/CPU/cpu/cpu.log
+    echo "开始查询" >> ~/CPU/cpu/cpu.log
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/cpu.log
-    top -bn 1 -i -c | head -6 >> ~/cpu/cpu.log
+    echo $ttime >> ~/CPU/cpu/cpu.log
+    top -bn 1 -i -c | head -6 >> ~/CPU/cpu/cpu.log
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/cpu.log
-    ps -eo pid,user,%cpu,%mem,args --sort -%mem | head -8 >> ~/cpu/cpu.log  
+    echo $ttime >> ~/CPU/cpu/cpu.log
+    ps -eo pid,user,%cpu,%mem,args --sort -%mem | head -8 >> ~/CPU/cpu/cpu.log  
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/cpu.log
-    ps -eo pid,user,%cpu,%mem,args --sort -%cpu | head -8 >> ~/cpu/cpu.log 
+    echo $ttime >> ~/CPU/cpu/cpu.log
+    ps -eo pid,user,%cpu,%mem,args --sort -%cpu | head -8 >> ~/CPU/cpu/cpu.log 
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/cpu.log
-    ps -eo pid,user,%cpu,%mem,args --sort -user | head -8 >> ~/cpu/cpu.log 
+    echo $ttime >> ~/CPU/cpu/cpu.log
+    ps -eo pid,user,%cpu,%mem,args --sort -user | head -8 >> ~/CPU/cpu/cpu.log 
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/cpu.log
-    sensors  >> ~/cpu/cpu.log
+    echo $ttime >> ~/CPU/cpu/cpu.log
+    sensors  >> ~/CPU/cpu/cpu.log
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/memory.log
-    free -m >> ~/cpu/memory.log
+    echo $ttime >> ~/CPU/cpu/memory.log
+    free -m >> ~/CPU/cpu/memory.log
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/memory.log
-    df -hl >> ~/cpu/memory.log
+    echo $ttime >> ~/CPU/cpu/memory.log
+    df -hl >> ~/CPU/cpu/memory.log
     sleep 0.1
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/memory.log
-    echo -e "结束查询" >> ~/cpu/memory.log
-    echo -e " " >> ~/cpu/memory.log
+    echo $ttime >> ~/CPU/cpu/memory.log
+    echo -e "结束查询" >> ~/CPU/cpu/memory.log
+    echo -e " " >> ~/CPU/cpu/memory.log
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-    echo $ttime >> ~/cpu/cpu.log
-    echo -e "结束查询" >> ~/cpu/cpu.log 
-    echo -e " " >> ~/cpu/cpu.log
+    echo $ttime >> ~/CPU/cpu/cpu.log
+    echo -e "结束查询" >> ~/CPU/cpu/cpu.log 
+    echo -e " " >> ~/CPU/cpu/cpu.log
     delFile
     sleep 0.1
 done
