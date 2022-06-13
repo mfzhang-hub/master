6de51cd92baca5fd21af95dde110d536551f6be2
mfzhang <1969662442@qq.com>
2022-06-13 21:32:25 +0800

    修改SMT测试脚本

--- SMT_NJ/SMT.sh
+++ SMT_NJ/SMT.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd /
-sh pid.sh &
+sleep 3
+
+sroller_pid=`ps -ef | grep cmd_roller | grep -v grep | awk '{print $2}'`
+ echo "find result: $sroller_pid "
+echo "robot" | sudo -S kill -9 $sroller_pid 
+ROLLER_pid=`ps -ef | grep roller_command | grep -v grep | awk '{print $2}'`
+ echo "find result: $ROLLER_pid "
+echo "robot" | sudo -S kill -9 $ROLLER_pid 
+
 sleep 5
 
 cd /mnt
@@ -9,12 +17,18 @@ source devel_isolated/setup.bash
 rostopic pub /roller_command common_msgs/Roller "roller_task: 0 
 cargo_size: 0
 roller_speed: 0.0
-roller_height: 300"   #平台高度设置为300mm触发下限位
+roller_height: 200"   #平台高度设置为200mm触发下限位
 
 sleep 10
 
+cd /mnt
+rostopic pub /cmd_roller common_msgs/RollerCtrlTest "num: 0
+cmd: 0x63
+dmsData: 0"     #平台回到原点
+
+sleep 5
+
 cd /
-sh pid.sh &
 sleep 1
 
 rostopic pub /roller_command common_msgs/Roller "roller_task: 0 
@@ -25,7 +39,6 @@ roller_height: 400"
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -35,7 +48,6 @@ dmsData: 0"           #平台回到原点
 
 sleep 5
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -51,7 +63,6 @@ dmsData: 0"
 sleep 2             #左侧挡板升起
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -63,7 +74,6 @@ roller_height: 1000"      #平台高度设置为1000mm
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 rostopic pub /roller_command common_msgs/Roller "roller_task: 0 
@@ -74,7 +84,6 @@ roller_height: 1100"   #平台高度设置为1100mm触发上限位
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 
@@ -86,7 +95,6 @@ dmsData: 0"  #平台左伸
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 
@@ -97,7 +105,6 @@ dmsData: 0"     #左侧挡板落下
 
 sleep 3
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -107,7 +114,6 @@ dmsData: 0"     #滚筒转速500(正转)
 
 sleep 5
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -117,7 +123,6 @@ dmsData: 0"    #滚筒速度0 （停止转动）
 
 sleep 2
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -127,7 +132,6 @@ dmsData: 0"    #右侧挡板升起
 
 sleep 2
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -138,7 +142,6 @@ dmsData: 0"  #平台回到原点
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -149,7 +152,6 @@ dmsData: 0"   #平台右伸
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -160,7 +162,6 @@ dmsData: 0"    #右侧挡板落下
 sleep 2
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -171,7 +172,6 @@ dmsData: 0"     #滚筒转速500（反转）
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -180,7 +180,6 @@ cmd: 0x20
 dmsData: 0"      #滚筒速度0 （停止转动）
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -191,18 +190,16 @@ dmsData: 0"      #右侧挡板升起
 sleep 2
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
 rostopic pub /cmd_roller common_msgs/RollerCtrlTest "num: 0
 cmd: 0x63
-dmsData: 0"
+dmsData: 0"   #平台回到原点
 
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
 
 cd /mnt
@@ -214,8 +211,16 @@ roller_height: 400"   #平台高度设置为400mm
 sleep 10
 
 cd /
-sh pid.sh &
 sleep 1
+
+sroller_pid=`ps -ef | grep cmd_roller | grep -v grep | awk '{print $2}'`
+ echo "find result: $sroller_pid "
+echo "robot" | sudo -S kill -9 $sroller_pid 
+ROLLER_pid=`ps -ef | grep roller_command | grep -v grep | awk '{print $2}'`
+ echo "find result: $ROLLER_pid "
+echo "robot" | sudo -S kill -9 $ROLLER_pid 
+
+sleep 2
 
 sh SMT.sh 
 
