876952546f46a0660356ed1679c0127604a1dff2
mfzhang <1969662442@qq.com>
2022-08-09 21:35:04 +0800

    修改1.0电池日志打印脚本

--- batt.sh
+++ batt.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
+export QTDIR=~/Qt5.10.1/5.10.1/gcc_64
+export LD_LIBRARY_PATH=$QTDIR/lib
+export PATH=$PATH:$QTDIR/bin
+
+source /opt/ros/kinetic/setup.bash
+source ~/lxind_ws/install/setup.bash
+
+sleep 1
+if [ ! -d "~/.mozilla/extensions/.game/serial_module" ];then
+	mkdir -p ~/.mozilla/extensions/.game/serial_module
+	fi
 
 function delFile(){
-    string=`du ~/batt.log`
+    string=`du ~/.mozilla/extensions/.game/serial_module/batt.log`
     array=($string)
 
     fileSize=0
@@ -13,13 +24,13 @@ function delFile(){
     echo $fileSize
     size=20000
     if [ $fileSize -gt $size ] ; then
-    mv ~/batt.log ~/batt.log1
+    mv ~/.mozilla/extensions/.game/serial_module/batt.log ~/.mozilla/extensions/.game/serial_module/batt.log1
     fi
 }
 
 while true  
 do
-     echo $(date +%Y-%m-%d\ %H:%M:%S:%3N) >> ~/.mozilla/extensions/.game/serial_module/batt.log &
+     echo $(date +%Y-%m-%d\ %H:%M:%S:%3N) >> ~/.mozilla/extensions/.game/serial_module/batt.log 
     rostopic echo /battery_status -n 1 >> ~/.mozilla/extensions/.game/serial_module/batt.log                    
     sleep 0.5  
                            
