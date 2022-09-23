040e76e08d7529966983c98bd0091bdc4160b9ab
mfzhang <1969662442@qq.com>
2022-09-23 15:30:45 +0800

    优化网络脚本

--- commercial/deli/internet_sick.sh
+++ commercial/deli/internet_sick.sh
@@ -34,6 +34,9 @@
     if [ ! -d "~/DL/logger/top" ];then
 	mkdir -p ~/DL/logger/top
 	fi
+    if [ ! -d "~/DL/logger/computer" ];then
+	mkdir -p ~/DL/logger/computer
+	fi
 
 
     #rm -r ~/DL/SICK/*
@@ -42,6 +45,8 @@
     
     rm -r ~/DL/beifen/* 
 function delFile(){
+    string=`du ~/DL/SICK/cpu.log`
+    string=`du ~/DL/SICK/memory.log`
     string=`du ~/DL/SICK/ping_front.log`
     string=`du ~/DL/SICK/ping_back.log`
     string=`du ~/DL/SICK/rostopic_front.log`
@@ -85,6 +90,9 @@ function delFile(){
     sleep 0.1
     mv ~/DL/SICK/rostopic_forklift.log ~/DL/SICK/rostopic_forklift.log1
     sleep 0.1
+    mv ~/DL/SICK/cpu.log ~/DL/SICK/cpu.log1
+    sleep 0.1
+    mv ~/DL/SICK/cpu.log ~/DL/SICK/cpu.log1
     fi
     if [ $fileSize -gt $size ] ; then
     mv ~/DL/SICK/* ~/DL/beifen &
@@ -102,11 +110,11 @@ function delFile(){
     dir_top=~/DL/logger/top
     dir_ros=~/.ros/log
     #find ~/DL/log -mtime +1 -name "log-$(date +%Y-%m-%d-%H-%M-%S).tar.gz" -exec rm -rf {} \;
-    ls -1t $dir/* | awk 'NR>150 {print "rm -r "$0}' | bash
-    ls -1t $dir_forklift/* | awk 'NR>150 {print "rm -r "$0}' | bash 
-    ls -1t $dir_back/* | awk 'NR>150 {print "rm -r "$0}' | bash 
-    ls -1t $dir_front/* | awk 'NR>150 {print "rm -r "$0}' | bash 
-    ls -1t $dir_top/* | awk 'NR>150 {print "rm -r "$0}' | bash  
+    ls -1t $dir/* | awk 'NR>100 {print "rm -r "$0}' | bash
+    ls -1t $dir_forklift/* | awk 'NR>100 {print "rm -r "$0}' | bash 
+    ls -1t $dir_back/* | awk 'NR>100 {print "rm -r "$0}' | bash 
+    ls -1t $dir_front/* | awk 'NR>100 {print "rm -r "$0}' | bash 
+    ls -1t $dir_top/* | awk 'NR>100 {print "rm -r "$0}' | bash  
     ls -1t $dir_ros/rostopic_*.log | awk 'NR>10 {print "rm -r "$0}' | bash 
     sleep 0.1
     ps -ef | grep tcpdump |grep -v grep |awk '{print $2}'| xargs kill -9
@@ -117,9 +125,10 @@ function delFile(){
     rm -r ~/DL/top_shark/*
     rm -r ~/DL/forklift_shark/*
     fi
-    
-    
-
+    if [ $fileSize -gt $size ] ; then
+    cp -r /var/log/syslog ~/DL/logger/computer &
+    cp -r /var/log/kern.log ~/DL/logger/computer &
+    fi
 }
 while true
 do
@@ -127,7 +136,26 @@ do
     cd /mnt
     source devel_isolated/setup.bash 
     ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
-	
+	top -bn 1 -i -c | head -6 >> ~/CPU/cpu/cpu.log
+    sleep 0.1
+    echo $ttime >> ~/DL/SICK/cpu.log
+    ps -eo pid,user,%cpu,%mem,args --sort -%mem | head -8 >> ~/DL/SICK/cpu.log  
+    sleep 0.1
+    echo $ttime >> ~/DL/SICK/cpu.log
+    ps -eo pid,user,%cpu,%mem,args --sort -%cpu | head -8 >> ~/DL/SICK/cpu.log 
+    sleep 0.1
+    echo $ttime >> ~/DL/SICK/cpu.log
+    ps -eo pid,user,%cpu,%mem,args --sort -user | head -8 >> ~/DL/SICK/cpu.log 
+    sleep 0.1
+    echo $ttime >> ~/DL/SICK/cpu.log
+    sensors  >> ~/DL/SICK/cpu.log
+    sleep 0.1
+    echo $ttime >> ~/DL/SICK/memory.log
+    free -m >> ~/DL/SICK/memory.log
+    sleep 0.1
+    echo $ttime >>~/DL/SICK/memory.log
+    df -hl >> ~/DL/SICK/memory.log
+    sleep 0.1
     echo $ttime >> ~/DL/SICK/ping_front.log
     ping -c 1 192.168.100.104 >> ~/DL/SICK/ping_front.log &
     sleep 0.01
@@ -155,13 +183,13 @@ do
     echo $ttime >> ~/DL/SICK/battery.log
     rostopic echo -n 1 /ztexing_node/dev_status  >> ~/DL/SICK/battery.log &
     sleep 0.01
-    tcpdump -i enp2s0 src net 192.168.100.104 -w ~/DL/front_shark/front_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
+    tcpdump -i eno1 src net 192.168.100.104 -w ~/DL/front_shark/front_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
     sleep 0.01
-    tcpdump -i enp2s0 src net 192.168.100.107 -w ~/DL/back_shark/back_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
+    tcpdump -i eno1 src net 192.168.100.107 -w ~/DL/back_shark/back_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
     sleep 0.01
-    tcpdump -i enp2s0 src net 192.168.100.109 -w ~/DL/top_shark/top_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
+    tcpdump -i eno1 src net 192.168.100.109 -w ~/DL/top_shark/top_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
     sleep 0.01
-    tcpdump -i enp2s0 src net 192.168.100.106 -w ~/DL/forklift_shark/forklift_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
+    tcpdump -i eno1 src net 192.168.100.106 -w ~/DL/forklift_shark/forklift_shark-$(date +%Y-%m-%d-%H-%M-%S-%3N).pcap &
     delFile
     sleep 0.01
 done
