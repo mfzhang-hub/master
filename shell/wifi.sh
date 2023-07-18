e1aae18644f927c04103dce41d6343b433de7996
mfzhang <1969662442@qq.com>
2023-07-18 21:52:34 +0800

    新增wifi信息打印查询

---
+++ shell/wifi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ttime=`date +"%Y-%m-%d %H:%M:%S.%3N"`
+wifi_card=$(iwconfig | grep -o '^[^ ]*')
+
+while true
+do
+
+if [ -z "$wifi_card" ]; then
+    echo "no" >> ~/interface.txt
+else
+     wifi_name=$(iwconfig $wifi_card | grep ESSID | awk -F '"' '{print $2}')
+     ap_name=$(iwconfig $wifi_card | grep Access | awk -F ' ' '{print $6}')
+     signal_strength=$(iwconfig $wifi_card | grep Signal | awk -F '=' '{print $3}' | awk '{print $1}')
+     delay=$(ping -c 1 8.8.8.8 | grep time= | awk -F '=' '{print $4}' | awk -F ' ' '{print $1}')
+     gateway=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
+     network_card=$(lspci | grep -i network | awk -F 'Network controller: ' '{print $2}')
+    echo "$ttime wifi_card:$wifi_card wifi_name:$wifi_name ap_name:$ap_name signal_strength:$signal_strength dbm delay:$delay ms network_card:$network_card " >> ~/interface.txt
+fi
+
+done
