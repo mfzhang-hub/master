ca54086288df709615e2b1b0ba1ff07bc626725b
mfzhang <1969662442@qq.com>
2023-07-19 20:43:26 +0800

    判断用户输入的是否为IP地址

---
+++ shell/未整理脚本/pandun_ip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash  
+function check_ip(){  
+    IP=$1  
+    VALID_CHECK=$(echo $IP|awk -F. '$1< =255&&$2<=255&&$3<=255&&$4<=255{print "yes"}')  
+    if echo $IP|grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$">/dev/null; then  
+        if [ $VALID_CHECK == "yes" ]; then  
+            echo "$IP available."  
+        else  
+            echo "$IP not available!"  
+        fi  
+    else  
+        echo "Format error!"  
+    fi  
+}  
+check_ip 192.168.1.1  
+check_ip 256.1.1.1  
+
