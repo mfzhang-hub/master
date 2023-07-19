1493c070e1e016ce48fe37b224b58c670e820bb1
mfzhang <1969662442@qq.com>
2023-07-19 20:39:30 +0800

    批量操作多台服务器

--- shell/未整理脚本/ssh.sh
+++ shell/未整理脚本/ssh.sh
@@ -31,3 +31,37 @@ expect "$user@*"  {send "$cmd\r"}
 expect "$user@*"  {send "exit\r"}  
 expect eof  
 
+
+
+批量操作多台服务器
+
+#!/bin/bash  
+HOST_INFO=user_info.txt  
+for ip in $(awk '{print $1}' $HOST_INFO)  
+do  
+    user=$(awk -v I="$ip" 'I==$1{print $2}' $HOST_INFO)  
+    pass=$(awk -v I="$ip" 'I==$1{print $3}' $HOST_INFO)  
+    expect login.exp $ip $user $pass $1  
+done  
+Linux主机SSH连接信息：  
+# cat user_info.txt  
+192.168.1.120 root 123456  
+创建10个用户，并分别设置密码，密码要求10位且包含大小写字母以及数字，最后需要把每个用户的密码存在指定文件中  
+```bash  
+#!/bin/bash  
+##############################################################  
+#创建10个用户，并分别设置密码，密码要求10位且包含大小写字母以及数字  
+#最后需要把每个用户的密码存在指定文件中#前提条件：安装mkpasswd命令  
+##############################################################  
+#生成10个用户的序列（00-09）  
+for u in `seq -w 0 09`do  
+ #创建用户  
+ useradd user_$u  
+ #生成密码  
+ p=`mkpasswd -s 0 -l 10`   
+ #从标准输入中读取密码进行修改（不安全）  
+ echo $p|passwd --stdin user_$u  
+ #常规修改密码  
+ echo -e "$p\n$p"|passwd user_$u  
+ #将创建的用户及对应的密码记录到日志文件中  
+ echo "user_$u $p" >> /tmp/userpassworddone  
