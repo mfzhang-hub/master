0cb013a2c440f99966df8b4efaac38d8a34b65b9
mfzhang <1969662442@qq.com>
2023-12-07 18:42:09 +0800

    更新wifi网络信息查询

--- shell/laser_zte.sh
+++ shell/laser_zte.sh
@@ -596,7 +596,7 @@ previous_metric=$current_metric
 current_metric=$(ip route show | grep "default via $target_gateway" | awk '{print $NF}' | sort -n | head -n 1)
 connected_devices=$(iw dev $wifi_card station dump | grep "Station" | wc -l)
 ap_info=$(iwlist $wifi_card scanning 1 essid "$(iwgetid -r)" 2>&1)
-iwlist_ap=$(echo "$ap_info" | grep "IE: IEEE 802.11" | awk -F ':' '{print $2}')
+iwlist_ap=$(echo "$ap_info" | grep -F 'IEEE 802.11' | awk -F ':' '{print $2}')
 freq=$(iwconfig $wifi_card | grep -F 'Frequency:' | awk '{print $2}' )
 if [ $wifi_switch -eq 1 ]; then
 if [ -z "$wifi_card" ]; then
@@ -609,7 +609,7 @@ if [[ "$current_metric" != "$previous_metric" ]]; then
     ip route show | awk -v current="$current_metric" -v previous="$previous_metric" '($NF > previous) && ($NF < current) {print $NF}'
     echo "$ttime 网关Metric发生变化：$previous_metric -> $current_metric" >> $wifi_network 
     fi
-    if [[ $iwlist_ap == *"802.11ax"* ]];then
+    if [[ $iwlist_ap != *"IEEE 802.11"* ]];then
     echo "$ttime 当前连接网络支持wifi6网卡(Wifi6 can be connected.)！" >> $wifi_network  
     else
     echo "$ttime ERROR：当前连接网络不支持wifi6网卡(Wifi6 cannot be connected.)！" >> $wifi_network 
