90d4a0e0888a689357dd81df82de538f25df26b8
mfzhang <1969662442@qq.com>
2024-04-10 22:10:34 +0800

    修改explain文件说明

--- shell/laser_camera_usb.sh
+++ shell/laser_camera_usb.sh
@@ -430,7 +430,7 @@ echo '54mI5pys5Y+377yadjE3LeS8mOWMlue9keWNoeafpeivouaMh+S7pO+8jOWQjOatpeWinuWKoO
 
 echo "
 DEBUG: debug开关，当此参数改为true时则打印下述相关执行顺序日志；
-DEBUG_executions_number： 此脚本循环打印时间（单位：秒-注意：计数是start1/end1每次循时间：计时打印公式：Circulate配置的参数*4），当达到配置的数值“Circulate”时，停止打印脚本且输出“---end---”信息；
+DEBUG_executions_number： 此脚本循环打印时间（单位：秒-注意：计数是start1/end1每次循时间：计时打印公式：Circulate配置的参数*间隔时间），当达到配置的数值“Circulate”时，停止打印脚本且输出“---end---”信息；
 max_size： 网络数据包的单包保存大小（单位：字节，10000000=10mb）；
 max_size_all： 除网络数据包之外其他的日志文件单个保存大小（单位：字节，10000000=10mb）；
 max_box： 网络数据包的所在文件夹下的循环保存数量，超过这个数值会自动覆盖最早生成的文件包；
